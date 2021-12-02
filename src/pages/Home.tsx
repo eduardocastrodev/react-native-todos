7d52f95c6979cca87db98c99b721b872e0e44a3a
Eduardo Castro <tec.eduardocastro@gmail.com>
2021-12-01 21:14:45 -0300

    add feature alerts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { ItemWrapper } from '../components/ItemWrapper';
@@ -10,6 +10,16 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const repeatedTitle = tasks.find( item => item.title === newTaskTitle );
+    if ( repeatedTitle ) {
+      Alert.alert(
+        "Task já cadastrada", 
+        "Você não pode cadastrar uma task com o mesmo nome"
+      );
+
+      return;
+    }
+
     const newTask = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -32,9 +42,24 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    const updateTasks = tasks.filter( item => item.id !== id);
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não",
+          onPress: () => { return; }, 
+          style: "cancel"
+        },
+        { text: "Sim",
+          onPress: () => {
+            const updateTasks = tasks.filter( item => item.id !== id);
 
-    setTasks( updateTasks );
+            setTasks( updateTasks );
+          },
+        }
+      ]
+    );
   }
 
   return (
